ba168cbdfb7765d29cb5885f2fe6bd09935301b1
Tijmen Brommet <tijmen@gmail.com>
2017-06-30 15:12:18 +0100

    Move gemfile download task to class

--- Rakefile
+++ Rakefile
@@ -86,26 +86,7 @@ task :export_fragmentation do
 end
 
 task :download do
-  begin
-    sh "mkdir cache/gemfiles"
-    sh "rm cache/gemfiles/*"
-  rescue
-  end
-
-  applications = YAML.load(HTTP.get('https://raw.githubusercontent.com/alphagov/govuk-developer-docs/master/data/applications.yml'))
-  repos = applications.each do |application|
-    next if application["retired"]
-
-    repo_name = application.fetch('github_repo_name')
-    url = "https://raw.githubusercontent.com/alphagov/#{repo_name}/master/Gemfile.lock"
-    response = HTTP.get(url)
-
-    if response.code == 200
-      File.write("cache/gemfiles/#{repo_name}", response)
-    else
-      puts "Skipping #{repo_name}"
-    end
-  end
+  Gemfiles.download
 end
 
 task :download_versions do
--- lib/gemfiles.rb
+++ lib/gemfiles.rb
@@ -1,12 +1,38 @@
 class Gemfiles
+  DIRECTORY = "cache/gemfiles"
+
   def self.all
-    Dir.glob("../cache/gemfiles/*").map do |filename|
-      appname = filename.gsub("../cache/gemfiles/", "")
+    Dir.glob("#{DIRECTORY}/*").map do |filename|
+      appname = filename.gsub("#{DIRECTORY}/", "")
 
       file = File.read(filename)
       lockfile = Bundler::LockfileParser.new(file)
 
       [appname, lockfile]
+    end
+  end
+
+  def self.download
+    begin
+      sh "mkdir #{DIRECTORY}"
+      sh "rm #{DIRECTORY}/*"
+    rescue
+    end
+
+    applications = YAML.load(HTTP.get('https://raw.githubusercontent.com/alphagov/govuk-developer-docs/master/data/applications.yml'))
+    repos = applications.each do |application|
+      next if application["retired"]
+
+      repo_name = application.fetch('github_repo_name')
+      url = "https://raw.githubusercontent.com/alphagov/#{repo_name}/master/Gemfile.lock"
+      response = HTTP.get(url)
+
+      if response.code == 200
+        print '.'
+        File.write("#{DIRECTORY}/#{repo_name}", response)
+      else
+        puts "Skipping #{repo_name}"
+      end
     end
   end
 end
