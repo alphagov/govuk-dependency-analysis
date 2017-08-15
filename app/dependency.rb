50efdddc4c5cf20189708b72c78a56921c533907
Tijmen Brommet <tijmen@gmail.com>
2017-08-15 23:43:36 +0100

    Sort dependencies when displaying

--- app/dependency.rb
+++ app/dependency.rb
@@ -7,7 +7,7 @@ class Dependency
   end
 
   def depended_on
-    @depended_on ||= @data["depended_on"].map { |gem| Application.find(gem) }
+    @depended_on ||= @data["depended_on"].map { |gem| Application.find(gem) }.sort_by(&:name)
   end
 
   def einzelgem?
@@ -15,7 +15,7 @@ class Dependency
   end
 
   def depended_on_directly
-    @depended_on_directly ||= @data["depended_on_directly"].map { |gem| Application.find(gem) }
+    @depended_on_directly ||= @data["depended_on_directly"].map { |gem| Application.find(gem) }.sort_by(&:name)
   end
 
   def depended_on_indirectly
